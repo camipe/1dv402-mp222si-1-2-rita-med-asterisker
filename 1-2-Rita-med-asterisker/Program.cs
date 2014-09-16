dd28b65efdc722768c7ece98972afc832162b287
Micael Persson <micael.c.persson@gmail.com>
2014-09-16 14:49:24 +0200

    First commit for laboration 1.2

---
+++ 1-2-Rita-med-asterisker/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_2_Rita_med_asterisker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int row = 0; row < 5; row++)
+            {
+                if ((row % 2) == 0)
+                {
+                    Console.Write();
+                }
+                
+                for (int col = 0; col < 38; col++)
+                {
+                    Console.Write("* ");
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }  
+            
+            
+
+        }
+    }
+}
